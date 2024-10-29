5609317a09bb127f3d87bbcd05e57c928accdcb3
Savva <savva.shuliatev@iss.digital>
2024-10-30 01:56:48 +0300

    MIT License

--- Sources/UIBottomSheet/Utils/Comparable+clamped.swift
+++ Sources/UIBottomSheet/Utils/Comparable+clamped.swift
@@ -2,7 +2,12 @@
 //  Comparable+clamped.swift
 //  DynamicBottomSheetApp
 //
-//  Created by Savva Shuliatev.
+//  Modified based on code covered by the MIT License.
+//  Original code by Ilya Lobanov, available at https://github.com/super-ultra/UltraDrawerView.
+//  Copyright (c) 2019 Ilya Lobanov
+//
+//  Modifications by Savva Shuliatev, 2024
+//  This code is also covered by the MIT License.
 //
 
 import Foundation
