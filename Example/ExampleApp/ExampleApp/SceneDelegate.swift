f56d800c52ffb310f3f58f0decd00d9b72debb99
Savva <savva.shuliatev@iss.digital>
2024-11-01 02:49:52 +0300

    Example

---
+++ Example/ExampleApp/ExampleApp/SceneDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  SceneDelegate.swift
+//  ExampleApp
+//
+//  Created by Savva Shuliatev on 01.11.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {}
+
---
+++ Example/ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Savva Shuliatev on 01.11.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
