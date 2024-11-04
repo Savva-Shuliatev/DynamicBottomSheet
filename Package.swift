990c5e6a2110224931fdef9c84ecd73045c440d2
Savva <savva.shuliatev@iss.digital>
2024-11-04 17:25:52 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "UIBottomSheet",
-  platforms: [.iOS(.v14)],
+  platforms: [.iOS(.v13)],
   products: [
     .library(
       name: "UIBottomSheet",
