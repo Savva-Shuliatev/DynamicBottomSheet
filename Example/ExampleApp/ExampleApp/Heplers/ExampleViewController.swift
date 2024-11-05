e79a3ca2a45c7ecb1ae475cf23e2c3ac3223488a
Savva <savva.shuliatev@iss.digital>
2024-11-06 00:15:47 +0300

    Update ExampleViewController.swift

--- Example/ExampleApp/ExampleApp/Heplers/ExampleViewController.swift
+++ Example/ExampleApp/ExampleApp/Heplers/ExampleViewController.swift
@@ -2,7 +2,8 @@
 //  ExampleViewController.swift
 //  ExampleApp
 //
-//  Created by Savva Shuliatev on 05.11.2024.
+//  Copyright (c) 2024 Savva Shuliatev
+//  This code is covered by the MIT License.
 //
 
 import UIKit
