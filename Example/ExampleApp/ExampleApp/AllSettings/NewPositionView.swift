44aae7deb68951d925223ac147e166e8d3408820
Savva <savva.shuliatev@yandex.ru>
2024-11-29 23:56:29 +0300

    Update NewPositionView.swift

--- Example/ExampleApp/ExampleApp/AllSettings/NewPositionView.swift
+++ Example/ExampleApp/ExampleApp/AllSettings/NewPositionView.swift
@@ -6,4 +6,39 @@
 //  This code is covered by the MIT License.
 //
 
-import Foundation
+import SwiftUI
+import DynamicBottomSheet
+
+struct NewPositionView: View {
+
+  @State private var position: RelativePosition = .hidden
+
+  @State private var edgeType: EdgeType = .top
+
+  var body: some View {
+    List {
+      Section(header: Text("Edge")) {
+        Picker("Edge", selection: $edgeType) {
+            ForEach(EdgeType.allCases) { edge in
+                Text(edge.displayName).tag(edge)
+            }
+        }
+        .pickerStyle(SegmentedPickerStyle())
+      }
+    }
+  }
+}
+
+private enum EdgeType: String, CaseIterable, Identifiable {
+  case top
+  case fromTop
+  case middle
+  case fromBottom
+  case bottom
+  case hidden
+  case proportion
+
+  var id: String { self.rawValue }
+
+  var displayName: String { rawValue }
+}
