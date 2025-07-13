61cafa2ba89e1bd207524efbb52a5da718ee6283
Savva <savva.shuliatev@yandex.ru>
2025-07-13 04:00:47 +0300

    RubberTests

--- Sources/DynamicBottomSheet/Utils/Rubber.swift
+++ Sources/DynamicBottomSheet/Utils/Rubber.swift
@@ -14,11 +14,16 @@ import Foundation
 import CoreGraphics
 
 struct Rubber {
-  
+
+  @available(*, unavailable)
+  private init() {}
+
+  /// - Returns: The rubber band adjusted value, always less than the input for positive values
   static func bandClamp(_ x: CGFloat, coeff: CGFloat = 0.55, dim: CGFloat) -> CGFloat {
     return (1.0 - (1.0 / ((x * coeff / dim) + 1.0))) * dim
   }
 
+  /// - Returns: A value that stays within reasonable bounds but allows soft overflow with diminishing returns
   static func bandClamp(_ x: CGFloat, coeff: CGFloat = 0.55, limits: ClosedRange<CGFloat>) -> CGFloat {
     let clampedX = x.clamped(to: limits)
     let diff = abs(x - clampedX)
